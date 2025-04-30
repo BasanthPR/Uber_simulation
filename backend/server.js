851e1fa42b88b4d2406f036031092faebd8bf63e
Shubham Naik <shubhamnaik@Shubhams-MacBook-Air.local>
2025-04-29 18:54:43 -0700

    updated admin route

--- backend/server.js
+++ backend/server.js
@@ -5,58 +5,9 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import rideRoutes from "./routes/ride.js";
 import billingRoutes from "./routes/billing.js";
-<<<<<<< Updated upstream
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const dotenv = require('dotenv');
-// const authRoutes = require('./routes/auth');
-
-// dotenv.config();
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// // Middlewares
-// app.use(cors());
-// app.use(express.json());
-
-// // Routes
-// app.use('/api/auth', authRoutes);
-// app.get('/api/mapbox-token', (req, res) => {
-//   const token = process.env.MAPBOX_TOKEN;
-//   if (!token) {
-//     return res.status(500).json({ error: 'Token not found in environment' });
-//   }
-//   res.json({ token });
-// });
-
-// // Connect to MongoDB and run server
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log('MongoDB connected');
-//     app.listen(PORT, () =>
-//       console.log(`Server running on http://localhost:${PORT}`)
-//     );
-//   })
-//   .catch((err) => console.error('Connection error', err));
-
-
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-
-// Route Imports
-const authRoutes = require('./routes/auth');
-const profileRoutes = require('./routes/profileRoute'); // <-- newly added
-=======
 import adminAuthRoutes from "./routes/adminAuthRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
->>>>>>> Stashed changes
+import profileRoutes from "./routes/profileRoute.js"; 
 
 dotenv.config();
 
@@ -72,7 +23,7 @@ app.use('/api/billing', billingRoutes);
 app.use("/api/admin", adminAuthRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.use('/api/profile', profileRoutes); // <-- new route added
+app.use('/api/profile', profileRoutes); 
 
 // Mapbox Token Route
 app.get('/api/mapbox-token', (req, res) => {
